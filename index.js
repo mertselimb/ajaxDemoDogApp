c1b3f6dfd0c444d5289bc49b122d5c0725060582
Mert Selimbeyoğlu <mertselimb@users.noreply.github.com>
2018-02-26 12:06:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var btn = document.querySelector("#btn");
+var img = document.querySelector("#photo");
+
+
+btn.addEventListener("click", function () {
+    var XHR = new XMLHttpRequest();
+
+    XHR.onreadystatechange = function () {
+        if (XHR.readyState == 4 && XHR.status == 200) {
+            var data = JSON.parse(XHR.responseText);
+            var url = data.message
+            img.src = url;
+        }
+    }
+
+    XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
+    XHR.send();
+
+
+});
